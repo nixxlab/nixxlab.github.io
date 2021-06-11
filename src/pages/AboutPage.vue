267ae778bd3187a1e9beff533185c59effef1e6e
Nick Nizovtsev <nizovtsevnv@gmail.com>
2021-06-11 23:56:34 +0400

    Add main pages

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page padding>
+    <div class="row justify-center">
+      <div class="col-12 col-md-8">
+        <h2 class="subtitle is-3">Nixxlab is a compact IT lab specializing in a web development and support services of worldwide projects.</h2>
+        <h2 class="subtitle is-3">
+          We develop, implement and maintain mobile network data exchange and billing platforms, analytical systems, content shops and landing pages.
+        </h2>
+        <h2 class="subtitle is-3">
+          We use new technologies for providing a qualitative improvement of our services:
+        </h2>
+        
+        <div class="text-center">
+          <q-avatar square size="200px">
+            <img src="../assets/docker.png" class="grayscale">
+          </q-avatar>
+          <q-avatar square size="200px">
+            <img src="../assets/vuejs.png" class="grayscale">
+          </q-avatar>
+          <q-avatar square size="200px">
+            <img src="../assets/clickhouse.png" class="grayscale">
+          </q-avatar>
+          <q-avatar square size="200px">
+            <img src="../assets/rabbitmq.png" class="grayscale">
+          </q-avatar>
+          <q-avatar square size="200px">
+            <img src="../assets/postgresql.png" class="grayscale">
+          </q-avatar>
+          <q-avatar square size="200px">
+            <img src="../assets/redis.png" class="grayscale">
+          </q-avatar>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style scoped>
+  img.grayscale {
+    filter: grayscale(100%);
+    -webkit-filter: grayscale(100%);
+  }
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-page padding>
+    <div class="banner">
+      <div class="text-center">
+        <h1>Web development and support services</h1>
+        <h3>
+          Highload / Big data / Scaling
+        </h3>
+      </div>
+    </div>
+
+    <div class="ocean">
+      <div class="wave"></div>
+      <div class="wave"></div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+  .banner {
+    align-items: center;
+    display: flex;
+    height: 60vh;
+    justify-content: center;
+  }
+
+  .ocean {
+    background: #FF8049;
+    bottom: 0;
+    height: 5%;
+    left: 0;
+    overflow-x: clip;
+    position: absolute;
+    width: 100%;
+  }
+
+  .wave {
+    background: url(../assets/orange_wave.svg) repeat-x;
+    position: absolute;
+    top: -198px;
+    width: 6400px;
+    height: 198px;
+    animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
+    transform: translate3d(0, 0, 0);
+  }
+
+  .wave:nth-of-type(2) {
+    top: -175px;
+    animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
+    opacity: 1;
+  }
+
+  @keyframes wave {
+    0% {
+      margin-left: 0;
+    }
+
+    100% {
+      margin-left: -1600px;
+    }
+  }
+
+  @keyframes swell {
+    0%,
+    100% {
+      transform: translate3d(0, -25px, 0);
+    }
+
+    50% {
+      transform: translate3d(0, 5px, 0);
+    }
+  }
+</style>
